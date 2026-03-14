8e2c818773c4dace702cad27de7513d7a28b88e3
mahlet <mahlet2ambaye@gmail.com>
2026-03-14 16:14:29 +0000

    fix : hande appeal

--- src/routes/report.router.ts
+++ src/routes/report.router.ts
@@ -4,10 +4,15 @@ import { validateCategory, validateShop } from '../middleware/idvalidation.middl
 import { requireActiveSeller } from '../middleware/role.middleware.js';
 import { validateBody } from '../middleware/validate.middleware.js';
 import { ReportSchema } from '../validation/report.validation.js';
-import { createReport, getReportsforshop } from '../controllers/report.controller.js';
+import { createReport, getReportsforshop, handleAppeal, sendAppeal } from '../controllers/report.controller.js';
 export const reportRouter = Router();
 
 
 
 reportRouter.post('/:shopId', authMiddleware,validateShop(),validateBody(ReportSchema) ,createReport);
 reportRouter.get('/:shopId',authMiddleware,requireActiveSeller() ,validateShop(), getReportsforshop);
+reportRouter.post('/appeal/:shopId', authMiddleware,validateShop(),sendAppeal);
+
+// Admin
+// handle appeal
+reportRouter.post('/appeal/:id/handle', authMiddleware, handleAppeal);
